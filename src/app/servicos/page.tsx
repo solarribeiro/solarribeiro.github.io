c0156a9f71d94a6d4368df76cb10995ad15d4b85
nexusmidia <nexustvligacao@gmail.com>
2025-06-02 17:04:32 -0300

    feat: changed services photos

--- src/app/servicos/page.tsx
+++ src/app/servicos/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
             <WhatsappIcon />
             <Header />
             <section className="bg-[#25374f] text-white">
-                <section className="py-4 px-6 max-w-128 mx-auto">
-                    <div className="container mx-auto bg-slate-100 p-4 rounded-lg shadow-md text-center">
-                        <h2 className="text-4xl font-bold text-blue-900">Nossos Serviços</h2>
+                <section className="py-4 px-6 max-w-92 mx-auto">
+                    <div className="container mx-auto bg-slate-100 p-2 rounded-lg shadow-md text-center">
+                        <h2 className="text-2xl font-bold text-blue-900">Nossos Serviços</h2>
                     </div>
                 </section>
                 <div className="container mx-auto flex flex-col p-4 md:flex-row md:justify-center md:gap-32">
@@ -24,7 +24,7 @@ export default function Home() {
                         <Link href="https://api.whatsapp.com/send/?phone=5511982658073&text=Vim+pelo+website+e+gostaria+de+pedir+um+or%C3%A7amento.&type=phone_number" className="bg-transparent hover:bg-[#e5930d] outline-solid outline-white transition-all px-6 py-3 text-white font-bold rounded-full md:float-right md:mr-4">Saiba Mais</Link>
                     </div>
                     <div className="p-4 order-last md:order-first">
-                        <Image src="/exemplos_1_placa_solar.png" alt="Exemplos placa solar" width={500} height={500}></Image>
+                        <Image src="/exemplos_5_piscina.png" alt="Exemplos placa solar" width={500} height={500}></Image>
                     </div>
                 </div>
             </section>
@@ -36,7 +36,7 @@ export default function Home() {
                         <Link href="https://api.whatsapp.com/send/?phone=5511982658073&text=Vim+pelo+website+e+gostaria+de+pedir+um+or%C3%A7amento.&type=phone_number" className="bg-transparent hover:bg-[#e5930d] outline-solid outline-[#e5930d] transition-all px-6 py-3 text-gray-800 font-bold rounded-full md:float-right md:mr-4">Saiba Mais</Link>
                     </div>
                     <div className="p-4">
-                        <Image src="/exemplos_1_placa_solar.png" alt="Exemplos placa solar" width={500} height={500}></Image>
+                        <Image src="/exemplos_3_suporte.png" alt="Exemplos placa solar" width={500} height={500}></Image>
                     </div>
                 </div>
             </section>
@@ -44,11 +44,11 @@ export default function Home() {
                 <div className="container mx-auto flex flex-col p-4 md:flex-row md:justify-center md:gap-32">
                     <div className="text-center p-2 order-first md:order-last md:mt-16">
                         <h1 className="font-semibold mb-2 text-4xl">Consultoria Energética</h1>
-                        <p className="text-2xl font-light text-gray-400 mb-6 md:max-w-96">Avaliação de consumo e viabilidade de energia solar.</p>
+                        <p className="text-2xl font-light text-gray-400 mb-6 md:max-w-96">Avaliação de consumo e viabilidade de uso do aquecimento solar.</p>
                         <Link href="https://api.whatsapp.com/send/?phone=5511982658073&text=Vim+pelo+website+e+gostaria+de+pedir+um+or%C3%A7amento.&type=phone_number" className="bg-transparent hover:bg-[#e5930d] outline-solid outline-white transition-all px-6 py-3 text-white font-bold rounded-full md:float-right md:mr-4">Saiba Mais</Link>
                     </div>
                     <div className="p-4 order-last md:order-first">
-                        <Image src="/exemplos_1_placa_solar.png" alt="Exemplos placa solar" width={500} height={500}></Image>
+                        <Image src="/exemplos_4_consulta.png" alt="Exemplos placa solar" width={500} height={500}></Image>
                     </div>
                 </div>
             </section>
