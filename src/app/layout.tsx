a5b07d8964bb1ec16a9bf340e9c4717485a4ea82
nexusmidia <nexustvligacao@gmail.com>
2026-01-09 17:20:02 -0300

    feat: google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-C87HDK3Q33"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);} 
+gtag('js', new Date());
+
+gtag('config', 'G-C87HDK3Q33');`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
